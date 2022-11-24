c24f435f2d00045b9632632df921a2269c6af017
Maria Laura Rosano <marialaurarosano@gmail.com>
2022-11-24 13:07:03 -0300

    Updating ApolloClient URI to my own prod url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-9460.up.railway.app/", // my own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
